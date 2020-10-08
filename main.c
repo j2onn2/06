3401a11d8578b28a296b6fc2fb0d19cb40376d4c
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-10-08 16:39:22 +0900

    .

--- main.c
+++ main.c
@@ -3,15 +3,48 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square ( int a)
+int get_integer ()
 {
-	return (a*a)	;
+	int a	;
+	printf("값을  입력하시오 : ");
+	scanf("%i", &a)	;
+	
+	return a	;
 }
 
-int main ()
+
+
+
+int factorial (int b)
 {
-	int a = 2	;
-	a= square(a)	;
-	printf("a=%i\n",a)	;
-	
+	int res = 1	;
+	int i	;
+	for (i=1;	i<=b;	i++)	
+	{
+		res=res*i	;
+	}
+	return res;
 }
+
+int combination (int n, int r )
+{
+	int n1, n2, n3;
+	
+	n1 = factorial(n)	;
+	n2 = factorial(n-r)	;
+	n3 = factorial(r)	;
+	return (n1/(n2*n3))	;
+}
+
+int main (void)
+{
+	int n, r, result	;
+	
+	n=get_integer()	;
+	
+	r=get_integer()	;
+
+	result =combination(n,r)	;
+	printf("The result is %i\n", result)	;
+}
+
