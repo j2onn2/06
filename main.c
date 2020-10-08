e87e8c1946f5742dd924082ce7f478aec17319eb
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-10-08 15:41:35 +0900

    .

--- main.c
+++ main.c
@@ -2,20 +2,45 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void print_star()
+
+int sumTWO (int a, int b)
 {
-	int i	;
-	for(i=0;	i<10;	i++)
-		printf("*")	;
+	int output	;
+	output = a+b	;
+	return output	;
+
 }
 
+int square (int n)
+{
+	int output	;
+	output = n*n	;
+	return output;
+}
 
+int get_max(int x, int y)
+{
+	int output	;
+	if (x<y)
+		output = y	;
+	else if (x> y)
+		output = x	;
+	return output	;
+	 } 
 int main(void) {
 	
-	print_star();
-	print_star();
-	print_star();
-		
-		
-	return 0;
+	int sum	;
+	int squareresult	;
+	int maxresult	;
+	
+	sum = sumTWO(2,5)	;
+	printf("Sum result is %i\n", sum)	;
+	 
+	squareresult = square(5)	;
+	printf("square result is %i\n", squareresult)	;
+	
+	maxresult = get_max (11, 7)	;
+	printf("max result is %i\n", maxresult)	;
+	
+
 }
